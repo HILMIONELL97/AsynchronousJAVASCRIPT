d19160f40341d10d646373b6e2926c4f7623b172
hilmimohammed97@gmail.com <simo.1997.gov@gmail.com>
2020-04-16 11:46:41 +0100

    last update after live coding

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -1,21 +1,20 @@
 var req = new XMLHttpRequest();
 var button1 = document.getElementById('button1');
 var customer = document.getElementById('customer');
+button1.addEventListener("click", loadcustomer);
 
-
-button1.addEventListener("click", loadCustomer);
-
-function loadCustomer() {
-
-    if (req.readyState = 4 && req.status == 200) {
-        var DATA = JSON.parse(req.responseText);
-        for (var i in DATA) {
-            customer.innerHTML = "id: " + DATA.id + "<br>" + "name: " + DATA.name + "<br>" + "company: " + DATA.company + "<br>" + "phone: " + DATA.phone;
+function loadcustomer() {
+    req.onload = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            var DATA = JSON.parse(req.responseText);
+            for (var i in DATA) {
+                customer.innerHTML = "id:" + DATA.id + "<br>" + "name:" + DATA.name + "<br>" + "company:" + DATA.company + "<br>" + "phone:" + DATA.phone;
+            }
+        } else if (req.status == 404) {
+            console.log("ERROR 404")
         }
-    } else if (req.status == 404) {
-        console.log("ERROR 404")
     }
-    req.open('GET', 'customer.json');
+    req.open('get', 'customer.json');
     req.send();
 
 };
@@ -23,26 +22,25 @@ function loadCustomer() {
 
 var req1 = new XMLHttpRequest;
 var button2 = document.getElementById('button2');
-
+var customers = document.getElementById('customers');
 
 button2.addEventListener('click', loadCustomers);
 
 function loadCustomers() {
-
+    var output = '';
     req1.onload = function() {
         if (req1.readyState = 4 && req1.status == 200) {
-            var DATA1 = JSON.parse(req1.response)
-            var customers = document.getElementById('customers');
+            var DATA1 = JSON.parse(req1.responseText)
             for (var i in DATA1) {
-                customers += "id: " + DATA1[i].id + "<br>" + "name: " + DATA1[i].name + "<br>" + "company: " + DATA1[i].company + "<br>" + "phone: " + DATA1[i].phone;
+                output += "id: " + DATA1[i].id + "<br>" + "name: " + DATA1[i].name + "<br>" + "company: " + DATA1[i].company + "<br>" + "phone: " + DATA1[i].phone + "<hr>";
             }
-            document.getElementById('customers').innerHTML = customers
+            document.getElementById('customers').innerHTML = output
 
         } else if (req1.status == 404) {
             console.log("ERROR 404")
         }
     }
-    req1.open('get', 'customers.json', true);
+    req1.open('get', 'customers.json');
     req1.send();
 
 };
